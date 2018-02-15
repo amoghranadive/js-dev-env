7cf4aa692c6d3156f7571f985416398db43a9ae5
Amogh Ranadive <Amogh.Ranadive@redi.com>
2018-02-15 16:19:37 -0500

    linting

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@ import './index.css';
 import numeral from 'numeral';
 
 const courseValue = numeral(1000).format('$0,0.00');
-debugger;
-console.log(`Total cost: ${courseValue}`);
+
+console.log(`Total cost: ${courseValue}`); // eslint-disable-line no-console
