2c3cfba85cb4894efcd1a9f675b26cb0e4a6a75d
Amogh Ranadive <Amogh.Ranadive@redi.com>
2018-02-20 14:36:26 -0500

    bundling

---
+++ src/vendor.js
@@ -0,0 +1,8 @@
+/**
+ * This file contains references to vendor libraries.
+ * This avoids customers having to download a huge JS file anytime a line of code changes.
+ */
+
+/* eslint-disable no-unused-vars */
+
+import fetch from 'whatwg-fetch';
